52b90187e4db5202684f699e9f711a49024d35ec
la.onger <yuelang85@gmail.com>
2024-08-02 15:30:08 +0800

    init，proxy的功能已经实现

---
+++ src/main.rs
@@ -0,0 +1,153 @@
+//use std::env;
+use std::net::SocketAddr;
+//use std::str::Bytes;
+//
+
+use std::cell::Cell;
+use std::rc::Rc;
+
+use std::marker::PhantomData;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::thread;
+
+use tokio::io::{self, AsyncWriteExt};
+
+//use bytes::Bytes;
+use http_body_util::{
+    BodyExt,
+    Full,
+    BodyDataStream,
+    BodyStream,
+    StreamBody,
+};
+use hyper::server::conn::{
+    http1,
+    http2,
+};
+use hyper::service::service_fn;
+use hyper::{
+    body::{
+        Incoming as IncomingBody,
+        Body as HyperBody,
+        Frame,
+        Bytes,
+    },
+    header, Method, Request, Response, StatusCode
+};
+
+use hyper_tls::HttpsConnector;
+
+
+use tokio::net::{TcpListener, TcpStream};
+
+use hyper_util::{
+    rt::tokio::{
+        TokioIo,
+        TokioExecutor,
+    },
+    client::legacy::Client,
+};
+
+type GenericError = Box<dyn std::error::Error + Send + Sync>;
+type Result<T> = std::result::Result<T, GenericError>;
+type ResultIO<T> = std::result::Result<T, std::io::Error>;
+
+use http_body_util::combinators::BoxBody;
+
+
+async fn request(
+        url: String,
+        req_data: Full<Bytes>,
+        api_key: String,
+    ) ->  Result<Response<IncomingBody>> {
+    
+    let uri = url.parse::<hyper::Uri>().unwrap();
+
+    let authority = uri.authority().unwrap().clone();
+
+
+    //let auth_header_val = format!("Bearer {}", api_key);
+    let auth_header_val = format!("{}", api_key);
+
+    let body = BodyStream::new(req_data);
+
+    let https = HttpsConnector::new();
+    let client = Client::builder(TokioExecutor::new()).build::<_, BodyStream<Full<Bytes>>>(https);
+
+    let req = Request::builder()
+        .method(Method::POST)
+        .uri(url)
+        .header(hyper::header::HOST, authority.as_str())
+        .header(header::CONTENT_TYPE, "application/json")
+        .header("Authorization", &auth_header_val)
+        .body(body)
+        .unwrap()
+        ;
+
+    println!("Request proxy: {:?}", req);
+    req.headers().iter().for_each(|(name, value)| {
+        println!("{}: {}", name.as_str(), value.to_str().unwrap());
+    });
+
+    let res = client.request(req).await?;
+    return Ok(res);
+}
+
+async fn handler(req: Request<IncomingBody>) -> Result<Response<IncomingBody>> {
+
+    println!("Request Raw: {:?}", req);
+    req.headers().iter().for_each(|(name, value)| {
+        println!("{}: {}", name.as_str(), value.to_str().unwrap());
+    });
+
+    let api_key = req.headers().get("Authorization").unwrap().to_str().unwrap().to_string();
+
+    let uri_path = req.uri().path();
+
+    let uri = format!("https://api.openai.com/{}", uri_path).to_string();
+    
+    let whole_body = req.into_body().collect().await?.to_bytes();
+    let full_body = Full::new(whole_body);
+
+    let res = request(
+        uri,
+        full_body,
+        api_key,
+    ).await?;
+
+    println!("Response: {:?}", res);
+    res.headers().iter().for_each(|(name, value)| {
+        println!("{}: {}", name.as_str(), value.to_str().unwrap());
+    });
+
+    return Ok(res)
+
+}
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    pretty_env_logger::init();
+
+    let addr: SocketAddr = "0.0.0.0:8002".parse().unwrap();
+
+    let listener = TcpListener::bind(&addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            let service = service_fn(move |req| {
+                handler(req)
+            });
+
+            if let Err(err) = http1::Builder::new().serve_connection(io, service).await {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
+    };
+}
+
+
+
